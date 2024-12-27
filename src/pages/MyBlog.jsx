6d6d423c4603d467630fe42c974ff9824adcc215
mdmonauwarulislam <mdmonauwarulislam@gmail.com>
2024-12-27 18:48:08 +0530

    correction

--- src/pages/MyBlog.jsx
+++ src/pages/MyBlog.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import CreateBlog from "../components/Blog/CreateBlog";
 import noData from "../assets/noDataSvg.svg";
